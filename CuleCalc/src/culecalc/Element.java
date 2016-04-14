15a462b0fd96f28068186048de685db919f44cf7
aboley <WMHS_Lab67@WMHS_RM67-16.GreeneSchools.com>
2016-04-14 13:10:49 -0400

    Element.java Added Constructor.

--- CuleCalc/src/culecalc/Element.java
+++ CuleCalc/src/culecalc/Element.java
@@ -1,5 +1,22 @@
 package culecalc;
 
 public class Element extends Formula{
+    private String name;
+    private String symbol;
+    private int protons;
+    private int electrons;
+    private int valence;
+    private int neutrons;
+    private double mass;
     
+    public Element(){}
+    public Element(String name, String symbol, int protons, int electrons, int valence, int neutrons, double mass){
+        this.name = name;
+        this.symbol = symbol;
+        this.protons = protons;
+        this.electrons = electrons;
+        this.valence = valence;
+        this.neutrons = neutrons;
+        this.mass = mass;
+    }
 }
